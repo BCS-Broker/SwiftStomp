531a998ed8c8aed49eaf7f1eabc6a381b4442b48
Nikolay Leontev <bently_93@mail.ru>
2020-12-25 11:07:59 +0300

    Swift Package Manager

--- Package.swift
+++ Package.swift
@@ -17,7 +17,7 @@ let package = Package(
         .target(
             name: "SwiftStomp",
             dependencies: ["Starscream",
-                           "ReachabilitySwift"],
+                           "Reachability"],
             path: "SwiftStomp"
         )
     ]
